3ba0d5e0708b4b8db2237aee7a23be384d7ad741
Maciej Mezyk <mcj.mezyk@gmail.com>
2025-04-10 00:02:08 +0200

    Adjust code formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { createRoot } from "react-dom/client";
+import { createRoot } from 'react-dom/client'
 
 const App = () => {
   return (
     <div>
       <h1>Hello from React</h1>
       <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum ut dolorem
-        suscipit mollitia voluptatum blanditiis, iure atque amet laborum eos
-        cumque doloribus quos? Modi molestias corporis, commodi amet eaque eum.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum ut dolorem suscipit mollitia
+        voluptatum blanditiis, iure atque amet laborum eos cumque doloribus quos? Modi molestias
+        corporis, commodi amet eaque eum.
       </p>
     </div>
-  );
-};
+  )
+}
 
-const container = document.querySelector("#root");
-const root = createRoot(container);
+const container = document.querySelector('#root')
+const root = createRoot(container)
 
-root.render(<App />);
+root.render(<App />)
